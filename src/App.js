a4e35b5a2a4b15eb15ce7cb59a9dd292d207ff9d
Tania <taniasugiharto@gmail.com>
2022-03-03 00:17:31 +0700

    halo gembol

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          halo gembol
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
